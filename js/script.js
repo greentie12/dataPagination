b9c9ceb41df8a3e31d6185745f230da7881045a5
greentie12 <tdominguez@fordham.edu>
2021-07-12 10:30:23 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+*/
+import { data } from "./data.js";
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+
+let list = data;
+
+for (let x = 0; x < data.length; x++) {}
+
+const showPage = (list, page) => {};
+
+console.log(list);
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+
+// Call functions
